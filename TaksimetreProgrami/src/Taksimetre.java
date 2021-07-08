5eedbc7cb8142fa3bfb4dde96b493a804974b6d2
Merve Keçeli <56134222+mervekeceli@users.noreply.github.com>
2021-07-08 19:28:47 +0300

    Add files via upload
    Taksimetre ücretini hesaplayan program.

--- TaksimetreProgrami/src/Taksimetre.java
+++ TaksimetreProgrami/src/Taksimetre.java
@@ -8,13 +8,13 @@ public class Taksimetre {
          *Taksimetre açılış ücreti 10 TL'dir.
          * */
 
-        double km, tutar, kmBirimTutar = 2.20;
+        double km, tutar = 10, kmBirimTutar = 2.20;
 
         Scanner input = new Scanner(System.in);
         System.out.printf("Gidilen mesafe kaç kilometredir: ");
         km = input.nextDouble();
 
-        tutar = (km/1000) * kmBirimTutar;
+        tutar += (km/1000) * kmBirimTutar;
 
         //tutar 20 liradan küçük olduğunda 20 lira ödeme yapması gerekir.
         tutar = (tutar<20) ? 20 : tutar;
